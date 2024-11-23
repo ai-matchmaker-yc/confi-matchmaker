75dc7aa6fcbc9681a805ac5e7d981d90438f29d1
Adam Bowker <adambowker98@gmail.com>
2024-11-23 15:08:51 -0800

    force dpeloy

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // force deploy
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
